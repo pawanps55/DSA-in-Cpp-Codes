012fa7bc5045c146a1bcdeeb59ea33030b3ffbc8
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2024-03-29 11:32:51 +0530

    Create Print any array.cpp

---
+++ Print any array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int number[15]={2,7};
+int n=15;
+cout<<"Printing the Array :"<<endl;
+for(int i=0;i<=n;i++){
+    cout<<number[i]<<" ";
+}
+
+int fifth[10]={1};
+int j=10;
+cout<<"Printing the array"<<endl;
+for (int k = 0; k <j; k++)
+{
+    cout<<fifth[k]<<" ";
+}
+
+}
