c1d8e9fd474bf0ac74eafb3880d8e870387f5f91
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2024-03-29 11:09:22 +0530

    Create Find Greater Number.cpp

---
+++ Find Greater Number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+
+cout<<"Enter Value of a : ";
+cin>>a;
+
+cout<<"Enter value of b : ";
+cin>>b;
+
+if(a>b){
+    cout<<" A is greater than b"<<endl;
+}
+
+if(a<b){
+    cout<<"b is greater than a: "<<endl;
+
+} 
