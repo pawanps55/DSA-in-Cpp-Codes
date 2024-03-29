80ad0cb25a76f5948312dc301b9e4f19bf52b2ec
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2024-03-29 11:22:42 +0530

    Create Generate row x column by entering any number.cpp

---
+++ Generate row x column by entering any number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i=1;
+int n;
+
+cout<<"Enter Value of n:";
+cin>>n;
+
+while(i<=n){
+    int j=1;
+    while (j<=n)
+    {
+        cout<<i;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}}
